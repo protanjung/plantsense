4a65bb945c9516a6459876354d446dfaaf2d5e2c
Pandu Surya Tantra <pandustantra@gmail.com>
2023-07-05 20:28:57 +0700

    Update interface_database untuk update dan upsert

--- src/ps_interface/scripts/interface_database.py
+++ src/ps_interface/scripts/interface_database.py
@@ -3,6 +3,8 @@
 import rospy
 from ps_interface.srv import db_insert, db_insertResponse
 from ps_interface.srv import db_select, db_selectResponse
+from ps_interface.srv import db_update, db_updateResponse
+from ps_interface.srv import db_upsert, db_upsertResponse
 from ps_interface.srv import db_delete, db_deleteResponse
 import psycopg2
 import pandas as pd
@@ -22,6 +24,8 @@ class InterfaceDatabase():
         # =====ServiceServer
         self.srv_db_insert = rospy.Service("db_insert", db_insert, self.cllbck_srv_db_insert)
         self.srv_db_select = rospy.Service("db_select", db_select, self.cllbck_srv_db_select)
+        self.srv_db_update = rospy.Service("db_update", db_update, self.cllbck_srv_db_update)
+        self.srv_db_upsert = rospy.Service("db_upsert", db_upsert, self.cllbck_srv_db_upsert)
         self.srv_db_delete = rospy.Service("db_delete", db_delete, self.cllbck_srv_db_delete)
 
         if self.interface_database_init() == -1:
@@ -49,6 +53,18 @@ class InterfaceDatabase():
 
         res = db_selectResponse()
         res.response = response
+        return res
+
+    def cllbck_srv_db_update(self, req):
+        self.db_update(self.db_schema, req.table_name, req.columns, req.values, req.where)
+
+        res = db_updateResponse()
+        return res
+
+    def cllbck_srv_db_upsert(self, req):
+        self.db_upsert(self.db_schema, req.table_name, req.columns, req.values, req.primary_key)
+
+        res = db_upsertResponse()
         return res
 
     def cllbck_srv_db_delete(self, req):
@@ -87,9 +103,11 @@ class InterfaceDatabase():
         tbl_data_names = ["id", "name", "value", "timestamp", "timestamp_local"]
         tbl_data_parameters = ["SERIAL NOT NULL PRIMARY KEY", "VARCHAR NOT NULL", "FLOAT NOT NULL", "TIMESTAMP NOT NULL", "TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP"]
         self.db_create_table(self.db_schema, "tbl_data", tbl_data_names, tbl_data_parameters)
-        self.db_create_table(self.db_schema, "tbl_data_last", tbl_data_names, tbl_data_parameters)
         self.db_create_table(self.db_schema, "tbl_data_last60sec", tbl_data_names, tbl_data_parameters)
         self.db_create_table(self.db_schema, "tbl_data_last1800sec", tbl_data_names, tbl_data_parameters)
+        tbl_data_names = ["name", "value", "timestamp"]
+        tbl_data_parameters = ["VARCHAR NOT NULL PRIMARY KEY", "FLOAT NOT NULL", "TIMESTAMP NOT NULL"]
+        self.db_create_table(self.db_schema, "tbl_data_last", tbl_data_names, tbl_data_parameters)
 
         return 0
 
@@ -133,6 +151,36 @@ class InterfaceDatabase():
         response = self.myCursor.fetchall()
         self.myDatabase.commit()
         return pd.DataFrame(response, columns=columns).to_json(orient="split", indent=2)
+
+    def db_update(self, table_schema, table_name, columns, values, where):
+        sql = "UPDATE " + table_schema + "." + table_name + " SET "
+        for i in range(len(columns)):
+            sql += columns[i] + " = '" + str(values[i]) + "'"
+            if i != len(columns) - 1:
+                sql += ", "
+        if where != "":
+            sql += " WHERE " + where
+        self.myCursor.execute(sql)
+        self.myDatabase.commit()
+
+    def db_upsert(self, table_schema, table_name, columns, values, primary_key):
+        sql = "INSERT INTO " + table_schema + "." + table_name + " ("
+        for i in range(len(columns)):
+            sql += columns[i]
+            if i != len(columns) - 1:
+                sql += ", "
+        sql += ") VALUES ("
+        for i in range(len(values)):
+            sql += "'" + str(values[i]) + "'"
+            if i != len(values) - 1:
+                sql += ", "
+        sql += ") ON CONFLICT (" + primary_key + ") DO UPDATE SET "
+        for i in range(len(columns)):
+            sql += columns[i] + " = '" + str(values[i]) + "'"
+            if i != len(columns) - 1:
+                sql += ", "
+        self.myCursor.execute(sql)
+        self.myDatabase.commit()
 
     def db_delete(self, table_schema, table_name, where):
         sql = "DELETE FROM " + table_schema + "." + table_name
